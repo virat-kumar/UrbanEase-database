ad272085c71095f732a140393bba867930a99395
poojahkhapekar21 <poojahkhapekar21@gmail.com>
2025-11-08 18:43:43 -0600

    Updated query2_category_hierarchy.sql

--- queries/khapekar_pooja/query2_category_hierarchy.sql
+++ queries/khapekar_pooja/query2_category_hierarchy.sql
@@ -10,13 +10,22 @@ USE urbanease_shop;
 -- TODO: Write your complex query here
 -- Example: Display category hierarchy (parent-child) with product counts
 
-/*
-SELECT 
-    parent.name as parent_category,
-    child.name as child_category,
-    COUNT(p.product_id) as product_count
-FROM Categories parent
--- Add your JOINs and WHERE clauses
-;
-*/
 
+SELECT 
+  pcat.name AS parent_category,
+  ccat.name AS subcategory,
+  pr.title AS product_title,
+  pr.brand,
+  pi.url AS product_image
+FROM Categories ccat
+LEFT JOIN Categories pcat 
+  ON ccat.parent_id = pcat.category_id               -- Get parent category
+JOIN Products pr 
+  ON pr.category_id = ccat.category_id               -- Get products in the subcategory
+LEFT JOIN (
+  SELECT product_id, MIN(url) AS url                 -- Pick 1 image per product (lowest sort_order or first)
+  FROM ProductImages
+  GROUP BY product_id
+) pi ON pr.product_id = pi.product_id                -- Attach image
+ORDER BY pcat.name, ccat.name, pr.title
+LIMIT 10;                                            -- Show 10 entries
