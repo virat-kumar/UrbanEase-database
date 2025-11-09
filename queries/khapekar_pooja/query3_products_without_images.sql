f9c0f2b66e1533c3039af79adbb048c259c5bee8
poojahkhapekar21 <poojahkhapekar21@gmail.com>
2025-11-08 18:45:46 -0600

    Updated query3_products_without_images.sql

--- queries/khapekar_pooja/query3_products_without_images.sql
+++ queries/khapekar_pooja/query3_products_without_images.sql
@@ -10,14 +10,17 @@ USE urbanease_shop;
 -- TODO: Write your complex query here
 -- Example: Find products that don't have any images
 
-/*
-SELECT 
-    p.product_id,
-    p.title,
-    c.name as category_name,
-    p.brand
-FROM Products p
--- Add your JOINs and WHERE clauses
-;
-*/
 
+SELECT 
+  p.product_id,
+  p.title AS product_title,
+  p.brand,
+  c.name AS category_name
+FROM Products p
+LEFT JOIN ProductImages pi 
+  ON p.product_id = pi.product_id              -- Try to join image
+LEFT JOIN Categories c 
+  ON p.category_id = c.category_id             -- Get category info
+WHERE pi.product_id IS NULL                    -- Keep only those with no image match
+ORDER BY p.created_at DESC
+LIMIT 10;                                      -- Just 10 records for review
